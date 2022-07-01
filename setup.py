12651ab0717ff064bbbb79cd70b70d89321f884f
PraveenS17N <108527292+PraveenS17N@users.noreply.github.com>
2022-07-01 17:24:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pilpxi',
+    version='1.0',
+    packages=['pilpxi'],
+    url='',
+    license='',
+    author='Pickering Interfaces',
+    author_email='support@pickeirngtest.com',
+    description='Python wrapper for Pilpxi'
+)
